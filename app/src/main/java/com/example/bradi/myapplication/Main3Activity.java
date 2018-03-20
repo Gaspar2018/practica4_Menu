1d50665ef91ec2073430068537822a735126fb0a
Gaspar2018 <gboza2000@gmail.com>
2018-03-20 11:05:10 -0500

    1er Commit.. cometido

---
+++ app/src/main/java/com/example/bradi/myapplication/Main3Activity.java
@@ -0,0 +1,46 @@
+package com.example.bradi.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu m) {
+        getMenuInflater().inflate(R.menu.primer_menu,m);
+        return true ;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem i) {
+        // return super.onOptionsItemSelected(i);//
+        switch (i.getItemId())
+        {
+            case R.id.opcion1:
+                Intent intento1= new Intent(this,MainActivity.class);
+                startActivity(intento1);
+                return true;
+            case R.id.opcion2:
+                Intent intento2= new Intent(this,MainActivity.class);
+                startActivity(intento2);
+                return true;
+            case R.id.opcion3:
+                Intent intento3= new Intent(this,MainActivity.class);
+                startActivity(intento3);
+                return true;
+            default:
+                return super.onOptionsItemSelected(i);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/bradi/myapplication/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.bradi.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+ //Cntr + O
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu m) {
+        getMenuInflater().inflate(R.menu.primer_menu,m);
+        return true ;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem i) {
+       // return super.onOptionsItemSelected(i);//
+        switch (i.getItemId())
+        {
+            case R.id.opcion1:
+                Intent intento1= new Intent(this,MainActivity.class);
+                startActivity(intento1);
+                return true;
+            case R.id.opcion2:
+                Intent intento2= new Intent(this,Main2Activity.class);
+                startActivity(intento2);
+                return true;
+            case R.id.opcion3:
+                Intent intento3= new Intent(this,Main3Activity.class);
+                startActivity(intento3);
+                return true;
+            default:
+                return super.onOptionsItemSelected(i);
+        }
+
+     }
+}
